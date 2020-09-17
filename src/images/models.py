dee1c9689d792ea6aa7dba47d99515e466fb0678
oxfos <rinaldo_bertossa@hotmail.com>
2020-09-17 21:24:36 +0200

    Added upload_to=media in Image model ImageField.

--- src/images/models.py
+++ src/images/models.py
@@ -10,7 +10,7 @@ class Image(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='media')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True, db_index=True)
 
