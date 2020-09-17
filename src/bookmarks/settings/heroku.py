ced9369f0c786de12b2896108303cbdfee0cfe0c
oxfos <rinaldo_bertossa@hotmail.com>
2020-09-17 21:10:48 +0200

    Removed storage argument from ImageField.

--- src/bookmarks/settings/heroku.py
+++ src/bookmarks/settings/heroku.py
@@ -4,6 +4,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 DEBUG = False
 
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
