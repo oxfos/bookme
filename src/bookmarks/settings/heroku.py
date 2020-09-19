baaa09b293463ead13e0033d840ad08c48966973
oxfos <rinaldo_bertossa@hotmail.com>
2020-09-19 17:56:12 +0200

    Tweaked heroku settings.

--- src/bookmarks/settings/heroku.py
+++ src/bookmarks/settings/heroku.py
@@ -21,7 +21,7 @@ AWS_ACCESS_KEY_ID =  os.environ.get("AWS_ACCESS_KEY_ID", "") # set in heroku env
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "") # set in heroku env variables
 AWS_STORAGE_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "imarksbucket") # set in heroku as env variable
 S3DIRECT_REGION = 'eu-central-1'
-AWS_S3_CUSTOM_DOMAIN = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # SORL-THUMBNAIL SPECIFIC
 MEDIA_URL = AWS_S3_CUSTOM_DOMAIN + 'media/'
