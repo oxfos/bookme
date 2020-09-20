ef5d31f44e19b724c41837ed2f36a04e7c804988
oxfos <rinaldo_bertossa@hotmail.com>
2020-09-20 14:41:51 +0200

    Added image view counts.

--- src/images/views.py
+++ src/images/views.py
@@ -8,8 +8,12 @@ from django.views.decorators.http import require_POST
 from bookmarks.common.decorators import ajax_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from actions.utils import create_action
+import redis
+from django.conf import settings
 
+# connect to redis
 
+r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
 def image_home(request):
     # Landing page.
@@ -42,7 +46,10 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+    # increment total image views by 1
+    total_views = r.incr(f'image:{image.id}:views')
+    return render(request, 'images/image/detail.html', {'section': 'images',
+         'image': image, 'total_views': total_views})
 
 
 @ajax_required
