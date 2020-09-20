f9b830385d26c66715132c0fa148e0a6aa24b987
oxfos <rinaldo_bertossa@hotmail.com>
2020-09-20 14:57:41 +0200

    Commented out redis line in image_detail view.

--- src/images/views.py
+++ src/images/views.py
@@ -47,9 +47,9 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # increment total image views by 1
-    total_views = r.incr(f'image:{image.id}:views')
+    # total_views = r.incr(f'image:{image.id}:views')
     return render(request, 'images/image/detail.html', {'section': 'images',
-         'image': image, 'total_views': total_views})
+         'image': image})
 
 
 @ajax_required
