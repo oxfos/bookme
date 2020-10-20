39f714d560bac52361126cb1a78da177beb40e91
oxfos <rinaldo_bertossa@hotmail.com>
2020-10-20 10:16:07 +0200

    Added first test.

--- src/images/tests.py
+++ src/images/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from account.models import Profile
 
-# Create your tests here.
+
+class Images_urls_test(TestCase):
+
+    @classmethod
+    def setUpTestData(self):
+        # Set up data for the whole TestCase
+        self.olmo = User.objects.create_user(username='username', password='password')
+        self.olmo_prof = Profile.objects.create(user=self.olmo)
+
+    def test_rankings_url(self):
+        self.client.login(username='username', password='password')
+        response = self.client.get('/ranking/')
+        self.assertTemplateUsed(response, 'images/image/ranking.html')
+        # self.assertEqual(response.context['most_viewed'], 'most_viewed') there are no images!
